63947e302cb3c41483a71daa4bbbb7dbc0d07ba3
igoman1 <71361612+jonghohan93@users.noreply.github.com>
2022-06-06 16:29:40 +0900

    링크연습

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
+    
+
     return (
+        <>
         <div>
             홈화면
         </div>
+
+<Link to="/report">
+        <button>분석 리포트 보기</button>
+</Link>
+     
+        </>
     );
 };
+
 
 export default Home;
