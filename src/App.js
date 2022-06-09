7951627b160ed27418f110d957be50c70a74f5d9
준호B 한 <han.junho@junhoBui-MacBookPro.local>
2022-06-10 00:28:30 +0900

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import './App.css';
-import Header from './layout/WhiteHeader';
-import Menu from './layout/Menu';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import Result from './pages/Result';
-import React, {useState, useEffect} from "react";
-import ReactDOM from "react-dom";
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
+import Header from "./layout/WhiteHeader";
+import Home from "./pages/Home";
+import Menu from "./layout/Menu";
+import ReactDOM from "react-dom";
+import Result from "./pages/Result";
 
 function App() {
-
-  return (
-    <>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/report" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
-    </>
-
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/report" element={<Result />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,40 +1,48 @@
-import React, { useState } from 'react';
-import './input-form.css';
-import Chevron from '../asset/Chevron.svg';
+import "./input-form.css";
+
+import React, { useState } from "react";
+
+import Chevron from "../asset/Chevron.svg";
+import DropdownSample1 from "./DropdownSample1";
+import DropdownSample2 from "./DropdownSample2";
 
 const InputForm = () => {
     const [isClicked, setisClicked] = useState(false);
     const dropdownClickHandler = () => {
         setisClicked(true);
-    }
+    };
+
     return (
         <form>
-            <div className='question-wrapper'>
-                <div className='question'>
-                    진료과목을 알려주세요
-                </div>
-                <div className='dropdown-wrapper'>
-                    <div className='department' onClick={dropdownClickHandler}>
+            <div className="question-wrapper">
+                <div className="question">진료과목을 알려주세요</div>
+                <div style={{ marginTop: "50px" }}>직접 만든거</div>
+
+                <div className="dropdown-wrapper">
+                    <div className="department" onClick={dropdownClickHandler}>
                         <span>진료과목 검색</span>
-                    <img src={Chevron}/>
+                        <img src={Chevron} />
                     </div>
                 </div>
-                    {isClicked ? 
-                        <div className='department-list-wrapper'>
-                            <ul className='department-list'>
-                                <li>가정의학과</li>
-                                <li>내과</li>
-                                <li>소아청소년과</li>
-                                <li>이비인후과</li>
-                                <li>마취통증의학과</li>
-                            </ul>
-                        </div>
-                    : null }
+                {isClicked ? (
+                    <div className="department-list-wrapper">
+                        <ul className="department-list">
+                            <li>가정의학과</li>
+                            <li>내과</li>
+                            <li>소아청소년과</li>
+                            <li>이비인후과</li>
+                            <li>마취통증의학과</li>
+                        </ul>
+                    </div>
+                ) : null}
+                {/* <div style={{ marginTop: "50px" }}>라이브러리</div>
+                <DropdownSample2 /> */}
+
+                {/* <div style={{ marginTop: "50px" }}>커스텀 한거</div> */}
+                <DropdownSample1 />
             </div>
         </form>
     );
 };
 
 export default InputForm;
-
-  
