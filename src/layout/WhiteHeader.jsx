310a1f75218b3f1c6fb9c7493b9e70b599a285be
준호B 한 <han.junho@junhoBui-MacBookPro.local>
2022-06-23 23:48:07 +0900

    modify header

--- src/layout/WhiteHeader.jsx
+++ src/layout/WhiteHeader.jsx
@@ -9,16 +9,16 @@ const WhiteHeader = () => {
     return (
         <>
             <div className="white-header">
-                <div className="previous-page-button child">
+                <div className="previous-page-button child child-left">
                     <img className="chevron-left" src={chevronLeft} />
                     <Link to="/" className="previous-page-text">
                         돌아가기
                     </Link>
                 </div>
-                <div className="child img">
+                <div className="child child-center">
                     <img className="logo-white" src={logoWhite} />
                 </div>
-                <div className="child">
+                <div className="child child-right">
                     <span className="header-description">지역 분석 리포트</span>
                 </div>
             </div>
